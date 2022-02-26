16aad98009074bfc6e4affdfe3e71ae245586839
jywkim <jkim589@gmail.com>
2022-02-25 21:31:41 -0500

    convert login app class to a function and add useState hooks

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,8 +1,11 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 
-export default class App extends React.Component {
-  handleSubmit = e => {
+export default function App() {
+  const [submit, setSubmit] = useState(false);
+  const [value, setValue] = useState({ email: "", test: "" });
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(e.target.email.value);
 
@@ -16,7 +19,7 @@ export default class App extends React.Component {
       e.target.email.value === "admin@test.com" &&
       e.target.password.value === "123456"
     ) {
-      alert("Successfully logged in");
+      setSubmit(true);
       e.target.email.value = "";
       e.target.password.value = "";
     } else {
@@ -24,30 +27,53 @@ export default class App extends React.Component {
     }
   };
 
-  handleClick = e => {
+  const handleClick = (e) => {
     e.preventDefault();
-
     alert("Goes to registration page");
   };
 
-  render() {
-    return (
-      <div className="App">
-        <form className="form" onSubmit={this.handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="admin@test.com" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-          </div>
-          <button className="primary">Enter</button>
-        </form>
-        <button className="secondary" onClick={this.handleClick}>
-          Create a new account
-        </button>
-      </div>
-    );
-  }
+  const changeValue = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="App">
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input 
+            type="email" 
+            name="email" 
+            value={value.email}
+            placeholder="admin@test.com" 
+            onChange={changeValue}/>
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input 
+            type="password" 
+            name="password" 
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="test">Test</label>
+          <input 
+            type="test" 
+            name="test" 
+            value={value.test}
+            placeholder="test" 
+            onChange={changeValue}/>
+        </div>
+        <button className="primary">Enter</button>
+      </form>
+      <button className="secondary" onClick={handleClick}>
+        Create a new account
+      </button>
+      {submit && (
+        <p>
+          Success! {value.email} and {value.test} is valid!
+        </p>
+      )}
+    </div>
+  );
 }
