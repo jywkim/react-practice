c42fa3674d7fca7edc475503b6a1790e59edcc97
jywkim <jkim589@gmail.com>
2022-02-26 21:47:10 -0500

    tic tac toe: remove unused ReactDOM

--- src/components/tictactoe/index.js
+++ src/components/tictactoe/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
   
 function Square(props) {
@@ -119,13 +118,6 @@ export default class Game extends React.Component {
     }
   }
   
-  // ========================================
-  
-  // ReactDOM.render(
-  //   <Game />,
-  //   document.getElementById('root')
-  // );
-
   function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
