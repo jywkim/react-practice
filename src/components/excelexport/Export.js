534941b1f468fb925b47857215a0aa6840962dfc
jywkim <jkim589@gmail.com>
2022-03-02 20:36:37 -0500

    add filter drop down to excel export

--- src/components/excelexport/Export.js
+++ src/components/excelexport/Export.js
@@ -12,17 +12,40 @@ class Export extends React.Component {
       email: `abc${i}@gmail.com`, address: `000${i} street city, ST`, zipcode: `0000${i}`});
     }
     return custs;
-  }
+  };
   
   state = {
     customers: this.customers(),
-    fileName: 'Customers'
-  }
+    fileName: 'Customers',
+    filterChanged: false
+  };
+
+  changeFilterValue(e) {
+    this.setState({
+      filterChanged: true, 
+      filterValue: e.target.value});
+  };
 
   render() {
 
     return (
       <div className="Export">
+         <div className="row">
+          <div className="col-md-8">
+            <select 
+              name="filter" 
+              onChange={(e) => this.changeFilterValue(e)}
+            >
+              <option value="FIRST">ABC</option>
+              <option value="SECOND">DEF</option>
+              <option value="THIRD">GHI</option>
+              <option value="FOURTH">JKL</option>
+            </select>
+          </div>
+          <div className="col-md-4 center">
+            <span>{this.state.filterChanged ? 'FILTER ' + this.state.filterValue : ''}</span>
+          </div>
+        </div>
         <div className="row">
             <div className="col-md-8">
                 <h2>Customers</h2>
