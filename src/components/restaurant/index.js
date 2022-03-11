3b5f01ff3baf22adf29575079617c5d04842f679
jywkim <jkim589@gmail.com>
2022-03-10 19:38:39 -0500

    add unique meals logic

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -9,9 +9,30 @@ export default function App() {
   meals.newMeal("Filet-O-Fish", ["Bun", "Cheese", "Tartar Sauce", "Fish"]);
   meals.newMeal("Quarter Pounder", ["Onion", "Beef", "Cheese", "Bun"]);
 
+  let arrayMeals = meals.meals;
+  let mealsLen = arrayMeals.length;
+  let map = {};
+  let ingredients = "";
+  let counter = 0;
+  for (let i = 0; i < mealsLen; i++) {
+    ingredients = arrayMeals[i].ingredients.sort().join("");
+    if (!map[ingredients]) {
+      map[ingredients] = 0;
+    }
+    map[ingredients]++;
+  } 
+  let recipes = Object.values(map);
+  for (let i = 0; i < recipes.length; i++) {
+    if (recipes[i] === 1) {
+      counter++;
+    }
+  }
+
   return (
     <div className="AppX">
         <pre id="json">
+          Unique Meals: {counter}
+          <br></br><br></br>
           {JSON.stringify(meals, null, 4)}
         </pre>
     </div>
