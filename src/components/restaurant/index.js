a3e5e858dad1d9f06075ffed37c8f5fa6e28664e
jywkim <jkim589@gmail.com>
2022-03-11 22:17:36 -0500

    add topic occurrences in reviews logic

--- src/components/restaurant/index.js
+++ src/components/restaurant/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import Meals from "./Meals";
 
 export default function App() {
+  //Unique Meals
   let meals = new Meals();
   meals.newMeal("Big Mac", ["Bun", "Cheese", "Onion", "Beef"]);
   meals.newMeal("McChicken", ["Bun", "Lettuce", "Mayo", "Chicken"]);
@@ -28,13 +29,53 @@ export default function App() {
     }
   }
 
+  //Topic Occurences in Reviews
+  let topics = {};
+  topics["Business specialties"] = ["dessert", "desserts"];
+  topics["Price"] = ["low", "high", "price"];
+  topics["Michael Bryant"] = ["michael bryant"];
+  let reviews = [];
+  reviews.push("Michael Bryant was a good host, but I expected more desserts for the price.");
+  reviews.push("I loved the desserts, thank you Michael.");
+  reviews.push("Very high at 20 dollars per dessert. I rather get from Cheap Desserts.");
+
+  let mapOccurences = {};
+  let topicKeys = Object.keys(topics);
+  let topicValues = Object.values(topics);
+  for (let i = 0; i < reviews.length; i++) {
+    let review = reviews[i].toLowerCase();
+    for (let j = 0; j < topicValues.length; j++) {
+      if (topicValues[j].some(t => review.includes(t))) {
+        if (!mapOccurences[topicKeys[j]]) {
+          mapOccurences[topicKeys[j]] = 0;
+        }
+        mapOccurences[topicKeys[j]]++;
+      }
+    }
+  }
+
   return (
     <div className="AppX">
         <pre id="json">
           Unique Meals: {counter}
-          <br></br><br></br>
+          <br></br>
           {JSON.stringify(meals, null, 4)}
         </pre>
+        <br></br><br></br><br></br><br></br>
+        <pre id="json">
+          Topic Occurences in Reviews:
+          <br></br>
+          {JSON.stringify(mapOccurences, null, 4)}
+          <br></br><br></br>
+          Topics:
+          <br></br>
+          {JSON.stringify(topics, null, 4)}
+          <br></br><br></br>
+          Reviews:
+          <br></br>
+          {JSON.stringify(reviews, null, 4)}
+        </pre>
+        <br></br><br></br><br></br><br></br><br></br><br></br>
     </div>
   );
 }
