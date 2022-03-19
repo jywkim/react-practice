92a3c5c4e10c15256eae74c8ee622f580eb8bf77
jywkim <jkim589@gmail.com>
2022-03-19 17:52:52 -0400

    tic-tac-toe 1P: create initial table

--- src/components/tictactoe1P/index.js
+++ src/components/tictactoe1P/index.js
@@ -4,9 +4,27 @@ import './index.css';
 export default class Game extends React.Component {
     render() {
       return (
-        <div className="game">
-          
-        </div>
+          <div className="table-container">
+            <table className="table-tictactoe">
+              <tbody>
+                <tr>
+                  <td className="cell" id="0"></td>
+                  <td className="cell" id="1"></td>
+                  <td className="cell" id="2"></td>
+                </tr>
+                <tr>
+                  <td className="cell" id="3"></td>
+                  <td className="cell" id="4"></td>
+                  <td className="cell" id="5"></td>
+                </tr>
+                <tr>
+                  <td className="cell" id="6"></td>
+                  <td className="cell" id="7"></td>
+                  <td className="cell" id="8"></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
       );
     }
   }
