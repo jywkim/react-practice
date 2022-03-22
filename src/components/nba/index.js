f01ed00314da66eac4b789fcb78bc356e1dba564
jywkim <jkim589@gmail.com>
2022-03-21 20:09:26 -0400

    pull players data from nba api

--- src/components/nba/index.js
+++ src/components/nba/index.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, {Component} from "react";
 import "./index.css";
+import axios from 'axios';
 
-export default function App() {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: []
+    }
+  }
 
-  return (
-    <div className="App">
+  async componentDidMount() {
+    const url = "https://www.balldontlie.io/api/v1/players";
+    // const url = "https://www.balldontlie.io/api/v1/players?search=durant";
+    let result = null;
+    try {
+      result = await axios(url, {
+        headers: {
+          Accept: "application/json"
+        }
+      })
+    } catch(e) {
+      console.log(e);
+    }
+    this.setState({players: result.data.data});
+  }
 
-    </div>
-  );
+  render(){
+    const {players} = this.state;
+    let mappedArray = (players).map((players, key) => {
+      return(
+        <li key={key}>{players.first_name} {players.last_name} {players.height_feet ? players.height_feet + "'" + players.height_inches + '"': ''}</li>
+      )
+    });
+
+    return (
+      <div className="App">
+        {mappedArray}
+      </div>
+    );
+  }
 }
+
