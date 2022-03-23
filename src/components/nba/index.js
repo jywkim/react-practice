cf05d3fb737dadcccb045d4e060d718ba17c9cc7
jywkim <jkim589@gmail.com>
2022-03-22 20:22:41 -0400

    create nba player stats search form

--- src/components/nba/index.js
+++ src/components/nba/index.js
@@ -6,37 +6,93 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      players: []
+      // players: [],
+      playerName : null,
+      playerStats: {}
     }
   }
 
-  async componentDidMount() {
-    const url = "https://www.balldontlie.io/api/v1/players";
-    // const url = "https://www.balldontlie.io/api/v1/players?search=durant";
-    let result = null;
-    try {
-      result = await axios(url, {
-        headers: {
-          Accept: "application/json"
-        }
-      })
-    } catch(e) {
-      console.log(e);
-    }
-    this.setState({players: result.data.data});
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getPlayerId();
+    console.log(this.state.playerName);
   }
+
+  handleChange = (event) => {
+    const replace = event.target.value.split(" ").join("_");
+    if(replace.length > 0) {
+      this.setState({playerName: replace});
+    } else {
+      alert("Please type player's name!");
+    }
+  }
+
+  getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+    .then(async res => {
+      if (res.data.data[0] === undefined) {
+        alert("This player is either injured or hasn't played yet");
+      } else if (res.data.data.length > 1) {
+        alert("Please specify the name more");
+      } else {
+        await this.getPlayerStats(res.data.data[0].id);
+      }
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${playerId}`)
+    .then(async res => {
+      console.log(res.data.data);
+      this.setState({ playerStats: res.data.data[0] });
+    }).catch(err => {
+      console.log(err);
+    })
+  }
+
+  // componentDidMount() {
+  //   this.getPlayerId();
+  //   this.getPlayerStats();
+  // }
+
+  // async componentDidMount() {
+  //   const url = "https://www.balldontlie.io/api/v1/players";
+  //   // const url = "https://www.balldontlie.io/api/v1/players?search=durant";
+  //   let result = null;
+  //   try {
+  //     result = await axios(url, {
+  //       headers: {
+  //         Accept: "application/json"
+  //       }
+  //     })
+  //   } catch(e) {
+  //     console.log(e);
+  //   }
+  //   this.setState({players: result.data.data});
+  // }
 
   render(){
-    const {players} = this.state;
-    let mappedArray = (players).map((players, key) => {
-      return(
-        <li key={key}>{players.first_name} {players.last_name} {players.height_feet ? players.height_feet + "'" + players.height_inches + '"': ''}</li>
-      )
-    });
-
     return (
       <div className="App">
-        {mappedArray}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder="Please enter player's name"
+            >
+            </input>
+          </label>
+          <input type="submit" value="Submit"></input>
+        </form>
+        GP: {this.state.playerStats["games_played"]}<br></br>
+        PPG: {this.state.playerStats["pts"]}<br></br>
+        RPG: {this.state.playerStats["reb"]}<br></br>
+        APG: {this.state.playerStats["ast"]}
       </div>
     );
   }
