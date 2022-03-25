1c53ac4240baf7a27d98072aaa6b91364fa1657f
jywkim <jkim589@gmail.com>
2022-03-24 22:38:57 -0400

    convert nba app from class to function

--- src/components/nba/index.js
+++ src/components/nba/index.js
@@ -1,93 +1,91 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import "./index.css";
 import axios from 'axios';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      playerName : null,
-      playerStats: {}
-    }
-  }
+export default function App() {
+  const [submit, setSubmit] = useState(false);
+  const [value, setValue] = useState({ playerName: "", playerStats: {} });
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     if (e.target[0].value.length !== 0) {
-      this.getPlayerId();
+      setSubmit(true);
+      getPlayerId();
     } else {
       alert("Please type player's name");
     }
   }
 
-  handleChange = (event) => {
-    const replace = event.target.value.split(" ").join("_");
-    if(replace.length > 0) {
-      this.setState({playerName: replace});
-    }
+ const handleChange = (e) => {
+    setValue({playerName: e.target.value});
   }
 
-  getPlayerId = () => {
-    axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
+  const getPlayerId = () => {
+    axios.get(`https://www.balldontlie.io/api/v1/players?search=${value.playerName}`)
     .then(async res => {
+      console.log('getPlayerId', res.data.data);
       if (res.data.data[0] === undefined) {
         alert("This player does not exist");
       } else if (res.data.data.length > 1) {
         alert("Please specify the name more");
       } else {
-        await this.getPlayerStats(res.data.data[0].id);
+        await getPlayerStats(res.data.data[0].id);
       }
     }).catch(err => {
       console.log(err);
     })
   }
 
-  getPlayerStats = (playerId) => {
+  const getPlayerStats = (playerId) => {
     axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${playerId}`)
     .then(async res => {
+      console.log('getPlayerStats', res.data.data);
       if (res.data.data[0] === undefined) {
         alert("This player is either injured or hasn't played yet this season");
       } else {
-        this.setState({ playerStats: res.data.data[0] });
+        setValue({ playerName: value.playerName, playerStats: res.data.data[0] });
       }
     }).catch(err => {
       console.log(err);
     })
   }
 
-  cancelSearch = () => { 
+  const cancelSearch = () => { 
     document.getElementById("formPlayer").reset();
-    this.setState({ playerStats: {} });
+    setValue({ playerName: "", playerStats: {} });
   }
 
-  render(){
-    return (
-      <div className="App">
-        <form 
-          id="formPlayer"
-          onSubmit={this.handleSubmit}>
-          <input
-            id="inputPlayer"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            placeholder="Please enter player's name"
-          >
-          </input>
-          <input type="submit" name="submit" value="Submit"></input>
-          <input type="button" name="cancel" value="Cancel" onClick={this.cancelSearch}></input>
-        </form><br></br>
-        GP: {this.state.playerStats["games_played"]}<br></br>
-        PPG: {this.state.playerStats["pts"]}<br></br>
-        RPG: {this.state.playerStats["reb"]}<br></br>
-        SPG: {this.state.playerStats["stl"]}<br></br>
-        BPG: {this.state.playerStats["blk"]}<br></br>
-        FG%: {this.state.playerStats["fg_pct"] }<br></br>
-        3P%: {this.state.playerStats["fg3_pct"]}<br></br>
-        TPG: {this.state.playerStats["turnover"]}
-
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <form 
+        id="formPlayer"
+        onSubmit={handleSubmit}>
+        <input
+          id="inputPlayer"
+          name="playerName"
+          type="text"
+          value={value.playerName}
+          onChange={handleChange}
+          placeholder="Please enter player's name"
+        >
+        </input>
+        <button name="submit" value="Submit">Submit</button>
+        <button name="cancel" value="Cancel" onClick={cancelSearch}>Cancel</button>
+      </form><br></br>
+      {submit && value.playerStats && (
+        <div>
+          GP: {value.playerStats["games_played"]}<br></br>
+          PPG: {value.playerStats["pts"]}<br></br>
+          RPG: {value.playerStats["reb"]}<br></br>
+          APG: {value.playerStats["ast"]}<br></br>
+          SPG: {value.playerStats["stl"]}<br></br>
+          BPG: {value.playerStats["blk"]}<br></br>
+          FG%: {value.playerStats["fg_pct"] }<br></br>
+          3P%: {value.playerStats["fg3_pct"]}<br></br>
+          TPG: {value.playerStats["turnover"]}
+        </div>
+      )}
+    </div>
+  );
 }
 
