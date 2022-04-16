b13acafc06a747bb6e569874ebb5ad64b60d20c5
jywkim <jkim589@gmail.com>
2022-04-15 20:00:18 -0400

    add login to spotify link

--- src/components/music/index.js
+++ src/components/music/index.js
@@ -2,9 +2,21 @@ import React from "react";
 import "./index.css";
 
 export default function App() {
+  const CLIENT_ID = "clientid";
+  const REDIRECT_URI = "http://localhost:3000/";
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token";
 
   return (
     <div className="container">
+      <div className="App">
+        <header className="App-header">
+          <h1>Spotify React</h1>
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
+            Login to Spotify
+          </a>
+        </header>
+      </div>
       Music
     </div>
   );
