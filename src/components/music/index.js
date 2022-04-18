4fbd903b30ca8bfaa35e041b3c7636b402c291eb
jywkim <jkim589@gmail.com>
2022-04-18 17:49:43 -0400

    music: save token in local storage (token not lost on page reload)

--- src/components/music/index.js
+++ src/components/music/index.js
@@ -4,15 +4,28 @@ import axios from 'axios';
 
 export default function App() {
   const [clientID, setClientID] = useState('');
-  const REDIRECT_URI = "http://localhost:3000/";
+  const REDIRECT_URI = "http://localhost:3000/music";
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token";
   const BACKEND_URL = "http://localhost:8000/music";
 
+  const [token, setToken] = useState("");
+
   useEffect(() => {
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem("token");
+    if (!token && hash)
+    {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1];
+      
+      window.location.hash = "";
+      window.localStorage.setItem("token", token);
+      setToken(token);
+    }
+
     const getClientId = async () => {
       const response = await axios.get(BACKEND_URL);
-      setClientID(response.data)
+      setClientID(response.data);
     }
     getClientId();
   }, []);
@@ -22,9 +35,11 @@ export default function App() {
       <div className="App">
         <header className="App-header">
           <h1>Spotify React</h1>
+          {!token ?
           <a href={`${AUTH_ENDPOINT}?client_id=${clientID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
             Login to Spotify
           </a>
+          : <button>Logout</button>}
         </header>
       </div>
     </div>
