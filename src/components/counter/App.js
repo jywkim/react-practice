0b582bc8696a64c7c1e3c639dd5be7e1ade2d9ec
jywkim <jkim589@gmail.com>
2022-04-25 17:10:08 -0400

    counter: persist state in localStorage with useStickyState

--- src/components/counter/App.js
+++ src/components/counter/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "./Button";
 import "./style.css";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const useStickyState = (defaultValue, key) => {
+    const [value, setValue] = useState(() => {
+      const stickyValue = window.localStorage.getItem(key);
+      return stickyValue !== null
+        ? JSON.parse(stickyValue)
+        : defaultValue;
+    });
+    useEffect(() => {
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }, [key, value]);
+    return [value, setValue];
+  };
 
-  let incrementCount = () => {
+  const [count, setCount] = useStickyState(0, "count");
+
+  const incrementCount = () => {
     setCount(count + 1);
   };
 
-  let decrementCount = () => {
+  const decrementCount = () => {
     setCount(count - 1);
   };
 
