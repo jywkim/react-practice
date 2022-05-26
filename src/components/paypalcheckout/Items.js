167c81e4ecd1fbd7d2758fe6644d73ff0039a7a5
jywkim <jkim589@gmail.com>
2022-05-26 17:59:23 -0400

    paypal checkout: change item list into table

--- src/components/paypalcheckout/Items.js
+++ src/components/paypalcheckout/Items.js
@@ -17,13 +17,36 @@ export default function Items() {
         )
     }, []);
 
+    const handleClick = (e) => {
+      console.log(e);
+    }
+
     return (
       <div>
-        <ul>
-          {items.map((item, key) =>(
-            <li key={key}>{item.details.name}, ${item.details.price}</li>
-          ))}
-        </ul>
+        <table>
+          <tbody>
+            <tr className="payRow">
+              <th className="payName">Name</th>
+              <th className="payPrice">Price</th>
+              <th className="payCheck"></th>
+            </tr>
+          </tbody>
+          <tbody>
+            {items.map((item, key) =>(
+            <tr key={key} className="payRow">
+              <td className="payName">
+                {item.details.name}
+              </td>
+              <td className="payPrice">
+                {item.details.price}
+              </td>
+              <td className="payCheck">
+                <input type="checkbox" onClick={handleClick}></input>
+              </td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
 }
