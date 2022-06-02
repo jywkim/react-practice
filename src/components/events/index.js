9380cecb71998d9536873a2b2a53bbaecdaf13f9
jywkim <jkim589@gmail.com>
2022-06-02 17:48:42 -0400

    events: load event data from Ticketmaster

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
+import axios from 'axios';
 
 export default function App() {
   const [submit, setSubmit] = useState(false);
   const [value, setValue] = useState({ city: "", artist: "" });
+  const [events, setEvents] = useState([]);
+  const urlEvents = "https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=" + process.env.REACT_APP_TICKETMASTER_CONSUMER_KEY;
+
+  useEffect(() => {
+    const loadEvents = async () => {
+      const response = await axios.get(urlEvents);
+      const events = response.data._embedded.events;
+      if (events.length > 0)
+        setEvents(events);
+    }
+    loadEvents();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (events.length > 0) console.log(events);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [events]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
